1889a7a820687cb10d36a7b4a380c85f8abf845b
toheeb1996 <toheebakandeabdulrasaq@gmail.com>
2023-01-15 03:50:17 -0800

    I am making this project

---
+++ main.js
@@ -0,0 +1,17 @@
+const tic = (event) => {
+  // let b = "X";
+  // event.target.innerText = b;
+  // console.log(b)
+  console.log('t')
+
+}
+
+const ade = (event) => {
+
+let toe = [1,2,3,4,5];
+let tac = [];
+for (let i = 0; i < toe.length; i++) {
+  console.log(tac.push(i))
+
+}
+}
